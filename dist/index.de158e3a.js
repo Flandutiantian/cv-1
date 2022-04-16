6d9fa92d91acd5372ec7b836183025cf6b9ddb50
Sonia <1737438545@qq.com>
2022-04-16 19:30:08 +0800

    init

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,8 @@
+let demo = document.querySelector("#demo");
+let n = 1;
+demo.innerHTML = n; // setInterval(() =>{
+ //     n = n+1;
+ //     demo.innerHTML = n;
+ // },3000);
+
+//# sourceMappingURL=index.de158e3a.js.map
